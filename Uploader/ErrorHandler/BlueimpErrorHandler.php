0f4855383cd54715323a3979f874428d2b2da66c
januszmk <januszmk6@gazeta.pl>
2014-09-20 23:54:08 +0200

    translating error messages

--- Uploader/ErrorHandler/BlueimpErrorHandler.php
+++ Uploader/ErrorHandler/BlueimpErrorHandler.php
@@ -5,12 +5,21 @@ namespace Oneup\UploaderBundle\Uploader\ErrorHandler;
 use Exception;
 use Oneup\UploaderBundle\Uploader\ErrorHandler\ErrorHandlerInterface;
 use Oneup\UploaderBundle\Uploader\Response\AbstractResponse;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class BlueimpErrorHandler implements ErrorHandlerInterface
 {
+
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+	$this->translator = $translator;
+    }
+
     public function addException(AbstractResponse $response, Exception $exception)
     {
-        $message = $exception->getMessage();
+        $message = $this->translator->trans($exception->getMessage());
         $response->addToOffset(array('error' => $message), array('files'));
     }
 }
